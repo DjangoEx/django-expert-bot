542f25da0691dcee790e99e693ce7f8070571b0c
Mani <mani.mozaffar@gmail.com>
2023-04-10 23:11:56 +0300

    Fixed typo/Pep8

--- filters/group_access.py
+++ filters/group_access.py
@@ -3,11 +3,13 @@ from pyrogram.types import Message
 from pyrogram.enums import ChatMemberStatus
 
 
-async def is_owner(_, client: Client, m: Message):
+async def is_owner(client: Client, m: Message):
     chat_member = await client.get_chat_member(m.chat.id, m.from_user.id)
     return chat_member.status == ChatMemberStatus.OWNER
 
 
-async def is_administrator(_, client: Client, m: Message):
+async def is_administrator(client: Client, m: Message):
     chat_member = await client.get_chat_member(m.chat.id, m.from_user.id)
-    return chat_member.status in (ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR)
+    return chat_member.status in (
+        ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR
+    )
